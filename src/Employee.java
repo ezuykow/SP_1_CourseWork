0dae7fe6e171465583dad26f7f08a54c1cb82f21
ezuykow <zuy1996@gmail.com>
2022-08-25 16:42:56 +0300

    ALL DONE. Needed to testing Employee.java - commented

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,18 @@
 class Employee {
-    private static int count = 0; //Counter
+    private static int count = 0;
 
-    private int id;
+    private final int id;
     private String fullName;
     private double salary;
     private int dept;
 
+    /**
+     * Constructor for new object Employee
+     * @param fullName - full name of new Employee
+     * @param salary - salary of new Employee
+     * @param dept - department, when new Employee working
+     * Field id - unique id of new Employee. Initialized by static field count+1
+     */
     public Employee(String fullName, double salary, int dept) {
         this.fullName = fullName;
         this.salary = salary;
